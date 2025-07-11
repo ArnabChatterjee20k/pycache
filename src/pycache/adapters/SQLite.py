1f2787353e1d6c013e031896ee3e02a6e3da2e23
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2025-07-12 00:07:01 +0530

    feat: enhance SQLite adapter with async operations and update PyCache for async compatibility

--- src/pycache/adapters/SQLite.py
+++ src/pycache/adapters/SQLite.py
@@ -1,17 +1,119 @@
 import sqlite3 as sqlite
+import asyncio
+from threading import Thread
+from queue import SimpleQueue
+from dataclasses import dataclass
+from functools import partial, wraps
 from .Adapter import Adapter
 from ..datatypes.Datatype import Datatype
-from ..sql import SQL, Composed, Identifier, Literal, Placeholder
+from ..sql import SQL, Composed, Identifier, Placeholder
+
+
+@dataclass
+class OperationPayload:
+    future: asyncio.Future
+    action: callable
+
+
+_STOP_RUNNING_EXECUTOR = object()
+
+
+def _async_op(fn):
+    @wraps(fn)
+    async def wrapper(self: "SQLite", *args, **kwargs):
+        # binding the fn to the SQLite scope
+        return await self._execute(fn.__get__(self), *args, **kwargs)
+
+    return wrapper
 
 
 class SQLite(Adapter):
-    def connect(self):
-        # HACK: sqlite assumes you are opening a connection, executing all operations and closing them in the same thread
-        self._db = sqlite.connect(self._connection_uri, check_same_thread=False)
+    def __init__(self, *args):
+        super().__init__(*args)
+        self._operation_queue: SimpleQueue[OperationPayload] = SimpleQueue()
+        self._executor = Thread(target=self._run_executor, daemon=True)
+        self._running = False
 
-    def close(self):
-        self._db.close()
+    async def __aenter__(self):
+        # return await self
+        return await self.connect()
 
+    # HACK: not using it anymore and directly using the aenter
+    """
+        we cant use the following any more
+        db = await SQLite("db.db")
+        await db.set("test2","arnab")
+        print(await db.get("test2"))
+
+        we need to use the followings
+            db = await SQLite("db.db").connect()
+            await db.set("test2","arnab")
+            print(await db.get("test2"))
+        or
+            with async with SQLite("db.db") as con:
+                pass
+        as remove __await__ and return `await self` from __aenter__ which was returing iterator and __await__ scheduling that iterator 
+        on the event loop
+    """
+    # def __await__(self):
+    #     return self.connect().__await__()
+
+    async def __aexit__(self, *args):
+        await self.close()
+
+    def _stop(self):
+        self._running = False
+        self._operation_queue.put_nowait(_STOP_RUNNING_EXECUTOR)
+
+    async def _execute(self, fn, *args, **kwargs):
+        if not self._running:
+            raise ValueError("Connection is closed")
+        future = asyncio.get_event_loop().create_future()
+        action = partial(fn, *args, **kwargs)
+        self._operation_queue.put_nowait(OperationPayload(future=future, action=action))
+        return await future
+
+    def _run_executor(self):
+        def set_future_result(future: asyncio.Future, result):
+            future.set_result(result)
+
+        def set_future_exception(future: asyncio.Future, error):
+            future.set_exception(error)
+
+        while True:
+            try:
+                payload = self._operation_queue.get()
+                if payload is _STOP_RUNNING_EXECUTOR:
+                    break
+                result = payload.action()
+                payload.future.get_loop().call_soon_threadsafe(
+                    set_future_result, payload.future, result
+                )
+            except BaseException as e:
+                payload.future.get_loop().call_soon_threadsafe(
+                    set_future_exception, payload.future, e
+                )
+
+    async def connect(self):
+        self._running = True
+        self._executor.start()
+
+        def connector():
+            return sqlite.connect(self._connection_uri)
+
+        self._db = await self._execute(connector)
+        return self
+
+    async def close(self):
+        try:
+            await self._execute(self._db.close)
+        except BaseException as e:
+            raise e
+        finally:
+            self._stop()
+            self._executor.join()
+
+    @_async_op
     def create(self):
         stmt = Composed(
             [
@@ -41,9 +143,11 @@ class SQLite(Adapter):
         db.execute(stmt)
         db.commit()
 
+    @_async_op
     def create_index(self):
         pass
 
+    @_async_op
     def get(self, key: str):
         stmt = Composed(
             [
@@ -60,6 +164,7 @@ class SQLite(Adapter):
         row = cursor.execute(stmt, (key,)).fetchone()
         return self.to_value(row[0]) if row else None
 
+    @_async_op
     def set(self, key: str, value: bytes) -> None:
         stmt = Composed(
             [
@@ -81,6 +186,13 @@ class SQLite(Adapter):
         cursor.execute(stmt, (key, self.to_bytes(value)))
         self._db.commit()
 
+        stmt = SQL("SELECT last_insert_rowid()").to_string()
+        cursor = self._db.cursor()
+        cursor.execute(stmt)
+        row = cursor.fetchone()
+        return row[0] if row else None
+
+    @_async_op
     def batch_get(self, keys: list[str]):
         stmt = Composed(
             [
@@ -98,6 +210,7 @@ class SQLite(Adapter):
         rows = cursor.execute(stmt, keys).fetchall()
         return {row[0]: self.to_value(row[1]) for row in rows}
 
+    @_async_op
     def batch_set(self, key_values: dict[str, Datatype]) -> None:
         stmt = (
             SQL(
@@ -121,6 +234,7 @@ class SQLite(Adapter):
         cursor.executemany(stmt, data)
         self._db.commit()
 
+    @_async_op
     def delete(self, key: str) -> None:
         stmt = Composed(
             [
@@ -134,6 +248,7 @@ class SQLite(Adapter):
         cursor.execute(stmt, (key,))
         self._db.commit()
 
+    @_async_op
     def exists(self, key: str) -> bool:
         stmt = Composed(
             [
@@ -147,6 +262,7 @@ class SQLite(Adapter):
         row = cursor.execute(stmt, (key,)).fetchone()
         return row is not None
 
+    @_async_op
     def keys(self) -> list:
         stmt = (
             SQL("SELECT {key} FROM {table}")
@@ -157,6 +273,7 @@ class SQLite(Adapter):
         rows = cursor.execute(stmt).fetchall()
         return [row[0] for row in rows]
 
+    @_async_op
     def set_expire(self, key: str, expires_at) -> None:
         stmt = Composed(
             [
--- src/pycache/py_cache.py
+++ src/pycache/py_cache.py
@@ -1,4 +1,3 @@
-import asyncio
 from contextlib import asynccontextmanager
 from .adapters.Adapter import Adapter
 from .datatypes.Datatype import Datatype
@@ -9,54 +8,54 @@ class PyCache:
         self.adapter = adapter
 
     async def __aenter__(self):
-        await asyncio.to_thread(self.adapter.connect)
+        await self.adapter.connect()
+        await self.adapter.create()
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await asyncio.to_thread(self.adapter.close)
+        await self.adapter.close()
 
     async def set(self, key, value: Datatype | dict[str, Datatype]):
         if isinstance(value, Datatype):
-            return await asyncio.to_thread(self.adapter.set, key, value.value)
+            return await self.adapter.set(key, value.value)
 
         if isinstance(value, dict):
-            return await asyncio.to_thread(self.adapter.batch_set, key, value)
+            return await self.adapter.batch_set(value)
 
-        raise TypeError("Value is not an instance of dictionary of string and Datatype")
+        raise TypeError("Value must be a Datatype or a dictionary of string → Datatype")
 
     async def batch_set(self, value: dict[str, Datatype]):
         if isinstance(value, dict):
-            return await asyncio.to_thread(self.adapter.batch_set, value)
+            return await self.adapter.batch_set(value)
 
-        raise TypeError("Value is not an instance of dictionary of string and Datatype")
+        raise TypeError("Value must be a dictionary of string → Datatype")
 
     async def batch_get(self, keys: list[str]):
-        if isinstance(keys, list):
-            return await asyncio.to_thread(self.adapter.batch_get, keys)
+        if not isinstance(keys, list):
+            raise TypeError("keys must be a list of strings")
 
-        raise TypeError("keys must be a list of string")
+        return await self.adapter.batch_get(keys)
 
     async def get(self, key):
-        value = await asyncio.to_thread(self.adapter.get, key)
-        return value if value else value
+        return await self.adapter.get(key)
 
     async def delete(self, key):
-        return await asyncio.to_thread(self.adapter.delete, key)
+        return await self.adapter.delete(key)
 
     async def exists(self, key):
-        return await asyncio.to_thread(self.adapter.exists, key)
+        return await self.adapter.exists(key)
 
     async def keys(self):
-        return await asyncio.to_thread(self.adapter.keys)
+        return await self.adapter.keys()
 
     async def set_expire(self, key, expires_at):
-        return await asyncio.to_thread(self.adapter.set_expire, key, expires_at)
+        return await self.adapter.set_expire(key, expires_at)
 
     @asynccontextmanager
     async def session(self):
         try:
-            await asyncio.to_thread(self.adapter.connect)
-            await asyncio.to_thread(self.adapter.create)
+            await self.adapter.connect()
+            await self.adapter.create()
             yield self
         finally:
-            await asyncio.to_thread(self.adapter.close)
+            await self.adapter.close()
